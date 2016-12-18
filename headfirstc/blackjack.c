04846c8634ccb34e773bf88add862b0dccb5a0e7
deb <deb@debian>
2016-12-17 21:55:43 -0800

    adding all

--- headfirstc/blackjack.c
+++ headfirstc/blackjack.c
@@ -10,29 +10,35 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-    int val = 0;
-    if (card_name[0] == 'K') {
-        val = 10;
-    } else if (card_name[0] == 'Q') {
-        val = 10;
-    } else if (card_name[0] == 'J' ) {
-        val = 10;
-    } else if (card_name[0] == 'A' ) {
-        val = 11;
-    } else {
-        val = atoi(card_name);
+    while (card_name[0] != 'X') {
+        puts("Enter the card_name: ");
+        scanf("%2s", card_name);
+        int val = 0;
+    
+        switch(card_name[0]) {
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            case 'x':
+                continue;
+                break;
+            default:
+                val = atoi(card_name);
+                if (card_name > 10 || card_name < 1)
+                    continue;
+        }
+        if (val > 2 && val < 7 ) {
+            count++;
+        }
+        else if (val == 10) {
+            count--;
+        }
+        printf("Current count: %i\n", count);
     }
-    /*printf("The card value is: %i\n", val);*/
-    /* Check if the value is 3 to 6 */
-    if (val > 2 && val < 7 ) {
-        puts("Count has gone up");
-    }
-    /*Otherwise check if the card was 10, J, Q, or K */
-    else if (val == 10) {
-        puts("Count has gone down");
-    }
-
     return 0;
 }
